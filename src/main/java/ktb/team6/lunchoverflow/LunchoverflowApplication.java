9a65873ae51df58a2cc06611e017de4556df8010
youth <boysoeng@naver.com>
2024-08-08 11:43:33 +0900

    initial commit

---
+++ src/main/java/ktb/team6/lunchoverflow/LunchoverflowApplication.java
@@ -0,0 +1,13 @@
+package ktb.team6.lunchoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchoverflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchoverflowApplication.class, args);
+    }
+
+}
