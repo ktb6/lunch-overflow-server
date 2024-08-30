745852dc4284e03dd561c5733023d18fed107fed
ChangHoSe <sas1397@naver.com>
2024-08-30 15:08:21 +0900

    Feature: Optional 추가, Entity의 @Setter 제거

--- src/main/java/ktb/team6/lunchoverflow/food/entity/RestaurantEntity.java
+++ src/main/java/ktb/team6/lunchoverflow/food/entity/RestaurantEntity.java
@@ -7,11 +7,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
 @Table(name = "restaurant")
 public class RestaurantEntity {
 
