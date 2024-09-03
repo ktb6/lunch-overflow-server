60900b7b9c5371ddaaefe2781a8554b0d378d426
youth <boysoeng@naver.com>
2024-09-03 14:25:35 +0900

    feat: 기본 에러 응답 형식 설정

---
+++ src/main/java/ktb/team6/lunchoverflow/common/error/ErrorResponse.java
@@ -0,0 +1,29 @@
+package ktb.team6.lunchoverflow.common.error;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public record ErrorResponse(
+        int code,
+        String message,
+        String method,
+        String requestURI
+) {
+
+    public static ErrorResponse of(ErrorCode errorCode, HttpServletRequest request) {
+        return new ErrorResponse(
+                errorCode.getCode(),
+                errorCode.getMessage(),
+                request.getMethod(),
+                request.getRequestURI()
+        );
+    }
+
+    public static ErrorResponse of(HttpServletRequest request, ErrorCode errorCode, final String errorMessage) {
+        return new ErrorResponse(
+                errorCode.getCode(),
+                errorMessage,
+                request.getMethod(),
+                request.getRequestURI()
+        );
+    }
+}
