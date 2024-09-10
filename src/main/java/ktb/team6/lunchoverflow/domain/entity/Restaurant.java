b30ef1cb9d82eeafb1e654327413f7ae3d8c0aee
youth <boysoeng@naver.com>
2024-09-10 10:35:35 +0900

    [KLO-25] feat: 초기 데이터 저장 로직 구현

--- src/main/java/ktb/team6/lunchoverflow/domain/entity/Restaurant.java
+++ src/main/java/ktb/team6/lunchoverflow/domain/entity/Restaurant.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Id;
 import ktb.team6.lunchoverflow.domain.embedded.Address;
 import ktb.team6.lunchoverflow.domain.embedded.Location;
 import ktb.team6.lunchoverflow.domain.enums.FoodCategory;
-import ktb.team6.lunchoverflow.domain.service.dto.KakaoPlace;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -34,10 +34,13 @@ public class Restaurant {
     @Column(name = "phone")
     private String phone;
 
-    @Column(name = "restaurant_url")
-    private String url;
+    @Column(name = "naver_url")
+    private String naverUrl;
 
-    @Column(name = "kakao_place_id", nullable = false, unique = true)
+    @Column(name = "kakao_url")
+    private String kakaoUrl;
+
+    @Column(name = "kakao_place_id",  unique = true)
     private Long kakaoId;
 
     @Embedded
@@ -53,35 +56,16 @@ public class Restaurant {
     @Column(name = "food_category")
     private FoodCategory foodCategory;
 
-    private Restaurant(String name, Long distance, String phone, String url, Long kakaoId, Address address,
-                       Location location, String rawCategory) {
+    @Builder
+    public Restaurant(String name, Long distance, String phone, String naverUrl, String kakaoUrl, Address address,
+                      Location location, String rawCategory) {
         this.name = name;
         this.distance = distance;
         this.phone = phone;
-        this.url = url;
-        this.kakaoId = kakaoId;
+        this.naverUrl = naverUrl;
+        this.kakaoUrl = kakaoUrl;
         this.address = address;
         this.location = location;
         this.rawCategory = rawCategory;
-    }
-
-    public static Restaurant from(KakaoPlace kakaoPlace) {
-        return new Restaurant(
-                kakaoPlace.getPlaceName(),
-                kakaoPlace.getDistance(),
-                kakaoPlace.getPhone(),
-                kakaoPlace.getPlaceUrl(),
-                kakaoPlace.getKakaoId(),
-                Address.from(kakaoPlace.getRoadAddressName()),
-                Location.builder()
-                        .latitude(Double.valueOf(kakaoPlace.getLatitude()))
-                        .longitude(Double.valueOf(kakaoPlace.getLongitude()))
-                        .build(),
-                kakaoPlace.getCategory()
-        );
-    }
-
-    public void updateFoodCategory(FoodCategory foodCategory) {
-        this.foodCategory = foodCategory;
     }
 }
